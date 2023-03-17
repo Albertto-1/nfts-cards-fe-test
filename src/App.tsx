8a0919a5260b5437db0d730dca05feae47c381d4
Albertto-1 <alberttomarvel@gmail.com>
2023-03-17 15:49:03 -0600

    initial test config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="bg-black h-screen w-screen overflow-y-scroll flex flex-col gap-2">
+            {/* NAV section */}
+            <section className={[
+                "p-4 z-10 relative",
+                "sticky top-0",
+                "bg-[#090A0D]/[0.5] backdrop-blur-[5px]",
+            ].join(" ")}>
+                <div className="max-w-8xl mx-auto flex items-center justify-between relative">
+                    <img className="cursor-pointer" src="https://www.alturanft.com/logo-svg.svg" alt="Altura" />
+                </div>
+            </section>
+
+            {/* CONTENT section */}
+            <section className={[
+                "max-w-8xl mx-auto relative",
+                "w-full",
+                "text-white text-lg",
+            ].join(" ")}>
+
+            </section>
+        </div>
+    );
 }
 
 export default App;
